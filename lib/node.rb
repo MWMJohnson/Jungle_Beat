275ad2e0bf25b9b5e76c6e9115539ddf6fa19b99
Matthew Johnson <matthewwmjohnson@gmail.com>
2023-03-26 14:35:01 -0600

    add the append_next_node method

--- lib/node.rb
+++ lib/node.rb
@@ -1,9 +1,11 @@
 class Node
-  attr_reader :data
-  attr_accessor :next_node
-  def initialize(data)
+  attr_reader :data, :next_node
+  def initialize(data, next_node = nil)
     @data = data
-    @next_node = nil
+    @next_node = next_node
   end
 
+  def append_next_node(node)
+    @next_node = node
+  end
 end
