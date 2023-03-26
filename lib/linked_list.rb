58614ed695abe768a7832e2289a4fe6d99dbea60
Matthew Johnson <matthewwmjohnson@gmail.com>
2023-03-26 14:33:59 -0600

    refactored append method

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -7,12 +7,32 @@ class LinkedList
   def append(data)
     if @head == nil
       @head = Node.new(data)
+      data
     else
+      # head is not nil anymore and we need to append a new node
+      # i need a pointer or a tracker for the nodes
+      # start pointer at head
+      # we need to keep cycling until the pointer.next_node == nil.
+      # create a new node and add data to that node.
+      
       current_node = @head
-      while(!current_node.next_node.nil?)
-        current_node = current_node.next_node
-      end
-      current_node.next_node = Node.new(data)
+      # require 'pry'; binding.pry
+        while current_node.next_node != nil
+        
+          current_node = current_node.next_node
+          # require 'pry'; binding.pry
+        end
+        
+      current_node.append_next_node(Node.new(data))
+      # require 'pry'; binding.pry
+
+
+
+      # current_node = @head
+      # while(!current_node.next_node.nil?)
+      #   current_node = current_node.next_node
+      # end
+      # current_node.next_node = Node.new(data)
     end
   end
 
@@ -26,9 +46,20 @@ class LinkedList
     counter 
   end
 
-
-
-
+  def to_string
+    beats = []
+    current_node = @head
+    if current_node.nil?
+    beats
+    else
+      while current_node.next_node != nil
+        beats << current_node.data
+        current_node = current_node.next_node
+      end
+      beats << current_node.data
+    end
+    beats.join(" ")
+  end
 end
 
 
