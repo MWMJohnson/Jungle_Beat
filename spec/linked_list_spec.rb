0bd033866ce7dc97819eebd77e69250dc1cb3f3f
Matthew Johnson <matthewwmjohnson@gmail.com>
2023-03-26 11:49:20 -0600

    fix append and count methods test

--- spec/linked_list_spec.rb
+++ spec/linked_list_spec.rb
@@ -13,29 +13,39 @@ RSpec.describe LinkedList do
     expect(list.head).to eq(nil)
   end
 
-  it "apppends new data" do
+  it "apppends new data to the end of the linked list" do
     list = LinkedList.new
-    list.append("doop")
-    #SOMETHING IS NOT RIGHT WITH LINE 21 `expect(list.head).to eq(list)`
-    #ASK!!!!!!!!
-    expect(list.head).to eq(list.head)
+    expect(list.append("doop").data).to eq("doop")
+    expect(list.head.data).to eq("doop")
+    list.append("wow")
+    expect(list.head.data).to eq("doop")
+    
   end
 
   it "checks the next node after appending" do
     list = LinkedList.new
     list.append("doop")
+    # expect(list.append("doop").data).to eq("doop")
+    expect(list.head.data).to eq("doop")
     expect(list.head.next_node).to eq(nil)
+    list.append("wow")
+    expect(list.head.next_node.data).to eq("wow")
+    
   end
   
   it "counts the nodes in a linked list" do
     list = LinkedList.new
     expect(list.head).to eq(nil)
-    expect(list.head.next_node).to eq(nil)
+    expect(list.count).to eq(0)
     list.append("doop")
     expect(list.head.next_node).to eq(nil)
-    expect(list.head.count).to eq(1)
-
-
+    expect(list.head.data).to eq("doop")
+    expect(list.count).to eq(1)
+    list.append("wow")
+    expect(list.head.next_node.data).to eq("wow")
+    expect(list.count).to eq(2)
+    list.append("say")
+    expect(list.count).to eq(3)
   end
 
 
